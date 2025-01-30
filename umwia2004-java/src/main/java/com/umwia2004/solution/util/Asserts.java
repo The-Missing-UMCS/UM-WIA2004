ebc45a54afac8daa14c9bd88bacb0b4a7792932a
fyiernzy <fyiernzy@gmail.com>
2025-01-30 18:00:52 +0800

    feat: add new Asserts static method to check dimension between arrays and matrices

--- umwia2004-java/src/main/java/com/umwia2004/solution/util/Asserts.java
+++ umwia2004-java/src/main/java/com/umwia2004/solution/util/Asserts.java
@@ -5,9 +5,31 @@ import org.apache.commons.collections4.CollectionUtils;
 import java.util.Collection;
 
 public class Asserts {
+    public static void sameDimension(int[][] matrix1, int[][] matrix2, Class<? extends Exception> clazz, String message) {
+        equals(matrix1.length, matrix2.length, clazz, message);
+        for (int i = 0; i < matrix1.length; i++) {
+            equals(matrix1[i].length, matrix2[i].length, clazz, message);
+        }
+    }
+
+    public static void sameDimension(int[] array, int[][] matrix, Class<? extends Exception> clazz, String message) {
+        for (int[] ints : matrix) {
+            equals(array.length, ints.length, clazz, message);
+        }
+    }
+
+    public static void equals(int value, int target, Class<? extends Exception> clazz, String message) {
+        isTrue(value == target, clazz, message);
+    }
+
+    public static <T extends Comparable<T>> void smallerThan(T value, T target, Class<? extends Exception> clazz, String message) {
+        isTrue(value.compareTo(target) < 0, clazz, message);
+    }
+
     public static <T extends Comparable<T>> void greaterThan(T value, T target, Class<? extends Exception> clazz, String message) {
         isTrue(value.compareTo(target) > 0, clazz, message);
     }
+
     public static void between(int value, int startInclusive, int endInclusive, Class<? extends Exception> clazz, String message) {
         isTrue(value >= startInclusive && value <= endInclusive, clazz, message);
     }
